efdb86e20ccadac065eeca563fd34f882a7bacd3
Megha0404 <megha_kumari1@epam.com>
2025-05-05 16:23:28 +0530

    New

---
+++ task/task03/versions.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.5.7"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.110.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
+  features {
+
+  }
+}
