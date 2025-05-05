2c97934f572b9402a2d942e74594c3d630194322
Megha0404 <megha_kumari1@epam.com>
2025-05-05 12:27:33 +0530

    Add terraform

---
+++ task03/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.27.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    subscription_id = "7e6a661c-8f9f-4149-b0df-5a01abea0a8e"
+ client_id = "f68c734d-be02-4bd5-a1b0-80751e7576b9"
+ client_secret = "i648Q~t127HGY4xCnmFmRAyXSSBqLl-IpIb~MbYK"
+ tenant_id = "b41b72d0-4e9f-4c26-8a69-f949f367c91d"
+  features {
+    
+  }
+}
